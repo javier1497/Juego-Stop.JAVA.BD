0cb2a68a85928ce207b7ee463d0136a647e3dece
Estudiante <Estudiante@TULDFPCLE8F058.FormacionSena.RED>
2018-09-06 12:25:44 -0500

    listo mompis jejejejje

--- juegoStop/src/logica/LogicaColor.java
+++ juegoStop/src/logica/LogicaColor.java
@@ -4,11 +4,44 @@
  * and open the template in the editor.
  */
 package logica;
-
+import clases.Color;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.Persistencia;
 /**
  *
  * @author Estudiante
  */
 public class LogicaColor {
+     public boolean VerificarColor(String Palabra){
+        String Verificar = "select  * from fruta where fruta.fruta like '"+Palabra+"'";
+        System.out.print(""+Verificar);
+        ResultSet resultado = null;
+        if(Persistencia.conectar()){
+            
+            try{
+                Persistencia.areadb = Persistencia.con.createStatement();
+                resultado = Persistencia.areadb.executeQuery(Verificar);
+                if(resultado.next()){
+                    return true;
+                }else{
+                    return false;
+                }
+            }catch (SQLException ex){
+                
+                Logger.getLogger(LogicaAnimal.class.getName()).log(Level.SEVERE,null, ex);
+                return false;
+            }
+            
+        
+        }else{
+            return false;
+        }
+        
+        
+    }
     
 }
+
--- juegoStop/src/logica/LogicaFruta.java
+++ juegoStop/src/logica/LogicaFruta.java
@@ -5,10 +5,47 @@
  */
 package logica;
 
+
+import clases.Fruta;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.Persistencia;
+
 /**
  *
  * @author Estudiante
  */
 public class LogicaFruta {
+     public boolean VerificarFruta (String Palabra){
+        String Verificar = "select  * from animal where animal.animal like '"+Palabra+"'";
+        System.out.print(""+Verificar);
+        ResultSet resultado = null;
+        if(Persistencia.conectar()){
+            
+            try{
+                Persistencia.areadb = Persistencia.con.createStatement();
+                resultado = Persistencia.areadb.executeQuery(Verificar);
+                if(resultado.next()){
+                    return true;
+                }else{
+                    return false;
+                }
+            }catch (SQLException ex){
+                
+                Logger.getLogger(LogicaAnimal.class.getName()).log(Level.SEVERE,null, ex);
+                return false;
+            }
+            
+        
+        }else{
+            return false;
+        }
+        
+        
+    }
     
 }
+    
+

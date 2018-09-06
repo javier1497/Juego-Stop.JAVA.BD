ce48da0bb77a61fd478fe985ac6cb2e307078d36
Estudiante <Estudiante@TULDFPCLE8F073.FormacionSena.RED>
2018-09-06 09:22:47 -0500

    Holaaaaa

--- juegoStop/src/clases/Fruta.java
+++ juegoStop/src/clases/Fruta.java
@@ -11,4 +11,39 @@ package clases;
  */
 public class Fruta {
     
+    private String fruta;
+   private int codFruta;
+   
+   public Fruta(){
+       
+   }
+
+    /**
+     * @return the fruta
+     */
+    public String getFruta() {
+        return fruta;
+    }
+
+    /**
+     * @param fruta the fruta to set
+     */
+    public void setFruta(String fruta) {
+        this.fruta = fruta;
+    }
+
+    /**
+     * @return the codFruta
+     */
+    public int getCodFruta() {
+        return codFruta;
+    }
+
+    /**
+     * @param codFruta the codFruta to set
+     */
+    public void setCodFruta(int codFruta) {
+        this.codFruta = codFruta;
+    }
+   
 }

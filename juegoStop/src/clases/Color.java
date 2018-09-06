f464e458441e2dc19c330661ebc9999bdca0b182
Estudiante <Estudiante@TULDFPCLE8F074.FormacionSena.RED>
2018-09-06 09:18:33 -0500

    Holaaaaa

--- juegoStop/src/clases/Color.java
+++ juegoStop/src/clases/Color.java
@@ -10,5 +10,39 @@ package clases;
  * @author Estudiante
  */
 public class Color {
-    
+     private int codColor;
+     private String Color;
+     
+     public Color(){
+         
+     }
+
+    /**
+     * @return the codColor
+     */
+    public int getCodColor() {
+        return codColor;
+    }
+
+    /**
+     * @param codColor the codColor to set
+     */
+    public void setCodColor(int codColor) {
+        this.codColor = codColor;
+    }
+
+    /**
+     * @return the Color
+     */
+    public String getColor() {
+        return Color;
+    }
+
+    /**
+     * @param Color the Color to set
+     */
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+     
 }
